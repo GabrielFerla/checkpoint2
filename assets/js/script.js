0226b30698821dc45ea36d634842f439803f3324
Gabriel Ferla <ferlagamer@hotmail.com>
2023-05-10 21:41:23 -0300

    implmenta mudanca de tema no index

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,22 @@ function clearCampos(){
 
 }
 
+function validaTema(){
+ 
+    //verifica qual classe esta no id
+    if(document.getElementById('changeColor').classList.contains("backgroundDark")){
+        //se for dark muda para light
+        document.getElementById('changeColor').classList.remove("backgroundDark");
+        document.getElementById('changeColor').classList.add("backgroundLight");
+
+    }else{
+        //se for light muda para dark
+        document.getElementById('changeColor').classList.remove("backgroundLight");
+        document.getElementById('changeColor').classList.add("backgroundDark");
+    }
+
+}
+
 
 const handlePhone = (event) => {
     let input = event.target
